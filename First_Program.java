8fa81cc7ad24447386b081fdefaac2d4d6ae5416
Zanjad <szanjad@ptc.com>
2020-09-23 15:51:00 +0530

    added new program to java code

---
+++ First_Program.java
@@ -0,0 +1,20 @@
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first program");
+		System.out.println("i am going to print the below pattern");
+		for(int i=0;i<4;i++)
+		{
+			
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(" "+"*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
